5c3363b037ed3665a8fc139faa89aaaf50bec7b0
Wildapfel <maxpetzold@yahoo.de>
2025-12-06 08:36:41 +0100

    Renamed data_viz to plots dir;Added a base plotter, wrapping the base plotting functino from matplotlib

---
+++ plotwrapper/plots/base.py
@@ -0,0 +1,85 @@
+from matplotlib.axes import Axes
+from ..boilerplate.remover import BoilerplateRemover
+
+
+
+class BasePlots:
+
+
+    def __init__(self, ax: Axes):
+        self._ax = ax
+
+
+    def bar(self, xlabel = None, ylabel = None, title = None, 
+            xlim = None, ylim = None, **kwargs):
+        
+        self._ax.bar(
+            edgecolor = "black",
+            **kwargs
+        )
+        BoilerplateRemover.reduce_boilerplate(
+            self._ax, 
+            title, 
+            xlabel, 
+            ylabel, 
+            xlim, 
+            ylim, 
+            **kwargs
+        )
+        
+
+    def hist(self, xlabel = None, ylabel = None, title = None, 
+             xlim = None, ylim = None, **kwargs):
+
+        self._ax.hist(
+            ec = "black", 
+            density = True, 
+            **kwargs
+        )
+        BoilerplateRemover.reduce_boilerplate(
+            self._ax, 
+            title, 
+            xlabel, 
+            ylabel, 
+            xlim, 
+            ylim, 
+            **kwargs
+        )
+        
+
+    def plot(self, x = None, y = None, xlabel = None, ylabel = None, 
+             title = None, xlim = None, ylim = None, **kwargs):
+
+        if x != None and y != None:
+            self._ax.plot(x, y, **kwargs)
+
+        elif y != None:
+            self._ax.plot(y, **kwargs)
+
+        BoilerplateRemover.reduce_boilerplate(
+            self._ax, 
+            title, 
+            xlabel, 
+            ylabel, 
+            xlim, 
+            ylim, 
+            **kwargs
+        )
+
+
+    def scatter(self, xlabel = None, ylabel = None, title = None, 
+                xlim = None, ylim = None, **kwargs):
+        
+        self._ax.scatter(
+            **kwargs
+        )
+        BoilerplateRemover.reduce_boilerplate(
+            self._ax, 
+            title, 
+            xlabel, 
+            ylabel, 
+            xlim, 
+            ylim, 
+            **kwargs
+        )
+
